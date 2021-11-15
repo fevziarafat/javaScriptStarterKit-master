2b581bbd3d1319cc51c875a32ffe5c141f201d6c
FevziArafat <fevzi.arafat@softbae.com.tr>
2021-11-15 07:50:02 +0300

    commit

---
+++ src/JS/1.js
@@ -0,0 +1,8 @@
+console.log("Merhaba")
+
+let konutKredileri=["konut kredileri", "Emlak konut kredileri", "Kamu konut kredileri"]
+
+console.log(konutKredileri)
+for (let index = 0; index < konutKredileri.length; index++) {
+    const element = console.log(konutKredileri[index]);
+}
